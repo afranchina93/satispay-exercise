6d82b42b733628b3e8809e70340e6a8c7c068860
afranchina <franchina.nino93@gmail.com>
2022-02-13 14:58:02 +0100

    Added files to gitignore

---
+++ src/main/java/it/satispay/satispayexercise/SatispayExerciseApplication.java
@@ -0,0 +1,13 @@
+package it.satispay.satispayexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatispayExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatispayExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/satispay/satispayexercise/SatispayExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package it.satispay.satispayexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatispayExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
